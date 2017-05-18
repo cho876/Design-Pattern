6a4f161e22f930bc4c091369fe033d4c25a676f5
Cho-PC\Cho <weapon114@naver.com>
2017-05-18 19:44:44 +0900

    <디자인패턴 1번째 과제> Strategy / Observer Pattern을 활용한 그림판 프로그램

---
+++ DP_Prac_01/src/OvalShapeBuilder.java
@@ -0,0 +1,95 @@
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+public class OvalShapeBuilder implements ShapeBuilder {
+
+	GrimPanModel model = null;
+	private ArrayList observerList;
+	public OvalShapeBuilder(GrimPanModel model){
+		this.model = model;
+		observerList = new ArrayList();
+	}
+	
+	@Override
+	public void RegisterObserver(Observer observer)
+	{
+		observerList.add(observer);
+	}
+	
+	@Override
+	public void RemoveObserver(Observer observer)
+	{
+		int index = observerList.indexOf(observer);
+		if(index>=0)
+			observerList.remove(index);
+	}
+	
+	@Override
+	public void NotifyObserver()
+	{
+		for(int i=0; i<observerList.size();i++)
+		{
+			Observer observer = (Observer)observerList.get(i);
+			observer.update(1);                        // update함수에 1을 넘겨줌으로써 count에 +1 추가 (count += 1)
+		}
+	}
+	
+
+	public void countChange()
+	{
+		NotifyObserver();
+	}
+	
+	@Override
+	public void performMousePressed(MouseEvent e) {
+		Point p1 = e.getPoint();
+		model.setMousePosition(p1);
+		model.setClickedMousePosition(p1);
+		countChange();
+		genEllipse2D();
+	}
+
+	/* (non-Javadoc)
+	 * @see hufs.cse.grimpan.strategy.ShapBuilder#performMouseReleased(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void performMouseReleased(MouseEvent e) {
+		Point p1 = e.getPoint();
+		model.setMousePosition(p1);
+
+		genEllipse2D();
+		if (model.curDrawShape != null){
+			model.shapeList
+			.add(new GrimShape(model.curDrawShape, model.getShapeStrokeWidth(),
+					model.getShapeStrokeColor(), model.isShapeFill(), model.getShapeFillColor()));
+			model.curDrawShape = null;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see hufs.cse.grimpan.strategy.ShapeBuilder#performMouseDragged(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void performMouseDragged(MouseEvent e) {
+		Point p1 = e.getPoint();
+		model.setLastMousePosition(model.getMousePosition());
+		model.setMousePosition(p1);
+
+		genEllipse2D();
+	}
+	
+	private void genEllipse2D(){
+		Point pi = model.getMousePosition();
+		Point topleft = model.getClickedMousePosition();
+		if (pi.distance(new Point2D.Double(topleft.x, topleft.y)) <= Util.MINPOLYDIST)
+			return;
+		Ellipse2D oval = new Ellipse2D.Double(
+				topleft.x, topleft.y,
+				pi.x-topleft.x, pi.y-topleft.y);
+		model.curDrawShape = oval;
+	}
+
+}
